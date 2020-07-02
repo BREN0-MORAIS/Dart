0c53559f06014d4555d8b8e937c16c0f13a63922
BRENO-MORAIS <brenofranciscomorais@gmail.com>
2020-07-01 21:32:36 -0400

    Estudos Dart

---
+++ Seção_4/aula_02-Variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  String nome = "Breno"; //armazenando o "Breno" na variavel nome
+  int idade = 19; //tipo inteiro
+  double salario = 100.05; // Tipo doulble
+  bool verdadeiro = true; // tipo booleano recebendo um Verdadeiro
+  bool falso = false; // tipo booleano recebendo um falso
+
+  //Moatrando os dados na Tela
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Salario: $salario");
+  print("seu dalario é Menor que 100? $falso");
+  print("seu salario é maior ou igual a 100? $verdadeiro");
+}
